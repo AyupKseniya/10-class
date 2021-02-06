c0621f6008a6540b0fbdc910992121df02b8f04c
Ксюша <61538751+AyupKseniya@users.noreply.github.com>
2021-02-06 09:52:05 +0700

    Create 9.42.py

---
+++ обработка_строк/9.42.py
@@ -0,0 +1,49 @@
+'''
+9.42
+sl=input("Введите слово")
+print(sl[::-1])
+'''
+'''
+9.44
+sl = input("введите слово")
+t = sl[::-1]
+print(t)
+'''
+'''
+9.46
+n=("_")
+print(n*8)
+'''
+'''
+9.48
+sl=input("введите слово")
+print("+"*4,sl,5*"_")
+'''       
+'''
+9.54
+s=input()
+buc="мин"
+k=0
+for i in s:
+    if i in buc:
+        k+=1
+print(k)        
+'''
+'''
+9.68
+s=input()
+sumvol="+-"
+k=0
+for i in s:
+    if i in sumvol:
+        k+=1
+print(k)
+'''
+9.70
+s=input()
+glas="уеыаоэяию"
+k=0
+for i in s:
+    if i in glas:
+        k+=1
+print(k)  
